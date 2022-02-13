bd0ead1ea9807abde078c85682d9ef21dcbaf760
Simon Boeger <simon@boegerweb.com>
2022-02-12 17:11:18 -0800

    fixed some errors, still many to go

--- main.cpp
+++ main.cpp
@@ -8,6 +8,41 @@ using namespace std;
 
 int tickRate = 60;
 
+class Entity {
+	public:
+		sf::Vector2f pos;
+		sf::Vector2f vel;
+		sf::Vector2f acc;
+		float maxSpeed;
+		Entity(float px, float py) {
+			pos.x = px;
+			pos.y = py;
+			vel.x = 0;
+			vel.y = 0;
+			acc.x = 0;
+			acc.y = 0;
+			maxSpeed = 2;
+		}	
+		Entity(float px, float py, float vx, float vy) {
+			pos.x = px;
+			pos.y = py;
+			vel.x = vx;
+			vel.y = vy;
+			acc.x = 0;
+			acc.y = 0;	
+			maxSpeed = 2;
+		}
+		Entity(float px, float py, float vx, float vy, float ax, float ay): Entity(px, py, vx, vy) {
+			pos.x = px;
+			pos.y = py;
+			vel.x = vx;
+			vel.y = vy;
+			acc.x = ax;
+			acc.y = ay;
+			maxSpeed = 2;
+		}
+};	
+
 ostream& operator<<(ostream& os, const sf::Vector2f v) {
 	os << "{" << v.x << ", " << v.y << "}";
 	return os;
@@ -18,24 +53,6 @@ ostream& operator<<(ostream& os, const Entity e) {
 	return os;
 }
 
-class Entity {
-	public:
-		sf::Vector2f pos;
-		sf::Vector2f vel(0, 0);
-		sf::Vector2f acc(0, 0);
-		Entity(float x, float y) {
-			pos.x = x;
-			pos.y = y;
-		}	
-		Entity(float px, float py, float vx, float vy): Entity(px, py) {
-			vel.x = vx;
-			vel.y = vy;
-		}
-		Entity(float px, float py, float vx, float vy, float ax, float ay): Entity(px, py, vx, vy) {
-			acc.x = ax;
-			acc.y = ay;
-		}
-};
 
 bool checkCollision(sf::RectangleShape shape1, sf::RectangleShape shape2) {
 	sf::Vector2f size1 = shape1.getSize();
@@ -51,59 +68,59 @@ class Game {
 		Entity player(200, 200);
 		sf::RectangleShape playerShape(sf::Vector2f(200, 200));
 
-	void update() {
-		//get input
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-			player.vel.x = -player.maxspeed;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-			player.vel.y = -player.maxspeed;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-			player.vel.x = -player.maxSpeed;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-			player.vel.y = player.maxSpeed;
-		}
-		else {
-			player.vel.x = 0;
-			player.vel.y = 0;
+		void update() {
+			//get input
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+				player.vel.x = -player.maxSpeed;
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+				player.vel.y = -player.maxSpeed;
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+				player.vel.x = -player.maxSpeed;
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+				player.vel.y = player.maxSpeed;
+			}
+			else {
+				player.vel.x = 0;
+				player.vel.y = 0;
+			}
+
+			//update pos and vel 
+			player.vel.x += player.acc.x;
+			player.vel.y += player.acc.y;
+			player.pos.x += player.vel.x;
+			player.pos.y += player.vel.y;
 		}
 
-		//update pos and vel 
-		player.vel.x += player.acc.x;
-		player.vel.y += player.acc.y;
-		player.pos.x += player.vel.x;
-		player.pos.y += player.vel.y;
-	}
+		void render() {
+			playerShape.setPosition(player.pos.x, player.pos.y);
+			
+			window.clear();
+			window.draw(playerShape);
+		}
 
-	void render() {
-		playerShape.setPosition(player.pos.x, player.pos.y);
-		
-		window.clear();
-		window.draw(playerShape);
-	}
-
-	void startGameLoop() {
-		sf::Clock clock;
-		while (window.isOpen()) {
-			sf::Event event;
-			while (window.pollEvent(event)) {
-				if (event.type == sf::Event::Closed) {
-					window.close();
+		void startGameLoop() {
+			sf::Clock clock;
+			while (window.isOpen()) {
+				sf::Event event;
+				while (window.pollEvent(event)) {
+					if (event.type == sf::Event::Closed) {
+						window.close();
+					}
 				}
-			}
 
-			update();
-			render();			
+				update();
+				render();			
 
-			sf::Time elapsed = clock.getElapsedTime();
-			while(elapsed.asMilliseconds() < 1000/tickRate) {
-				elapsed = clock.getElapsedTime();
+				sf::Time elapsed = clock.getElapsedTime();
+				while(elapsed.asMilliseconds() < 1000/tickRate) {
+					elapsed = clock.getElapsedTime();
+				}
+				clock.restart();
 			}
-			clock.restart();
 		}
-	}
 };
 
 int main() {
